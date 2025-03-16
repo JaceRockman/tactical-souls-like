c7a50727f0f2204f7c951d0147452f15d2e94ef3
Jace Rockman <jacemaverick5@gmail.com>
2025-03-16 15:13:32 -0500

    Implement combat position targeting
    When moving away, your combat position updates and attacks now send
    their position targeting information as well as their damage so
    whether an attack hits is now determined by those relative positions
    instead of using vulnerability states.

--- characters/corvant/combat/corvant.gd
+++ characters/corvant/combat/corvant.gd
@@ -8,11 +8,12 @@ var rng = RandomNumberGenerator.new()
 var frame_counter = 0
 var attack_count = 200
 var damage_state = 0
+var combat_position: Vector2 = Vector2(1, 0)
 @export var invulnerable = false
 @export var max_health = 5
 @export var current_health = 5
 
-signal damage(amount: int)
+signal damage(amount: int, target_positions: Array[Vector2])
 
 func _ready() -> void:
 	pass
@@ -36,16 +37,21 @@ func _process(delta: float) -> void:
 	if !anim.is_playing():
 		anim.play("corvant/idle")
 
-func attack(damage_quantity: int):
-	emit_signal("damage", damage_quantity)
+func attack(damage_quantity: int, target_positions):
+	emit_signal("damage", damage_quantity, target_positions)
 
 func plan_next_attack():
 	frame_counter = 0
 	attack_count = rng.randi_range(60, 150)
 
-func _on_damage(damage_amount: int) -> void:
-	if !invulnerable:
+func _on_damage(damage_amount: int, target_positions) -> void:
+	print(target_positions)
+	print(combat_position)
+	if target_positions.has(combat_position):
 		damage_state = 6
 		current_health -= damage_amount
 		corvant.set_modulate(Color(1, 0, 0, 1))
 		corvant.modulate
+
+func update_combat_position(movement_vector: Vector2):
+	combat_position += movement_vector
--- characters/ghosty/ghosty.gd
+++ characters/ghosty/ghosty.gd
@@ -4,6 +4,7 @@ extends AnimatedSprite2D
 @export var current_health = 5
 @export var damage_state = 0
 @export var invulnerable = false
+var combat_position: Vector2 = Vector2(1, 0)
 
 @onready var state_machine = $StateMachine
 @onready var astar_grid: AStarGrid2D = AStarGrid2D.new()
@@ -15,7 +16,8 @@ enum Direction {
 
 signal start_movement(direction: String)
 signal finish_movement(direction: String)
-signal damage(amount: int)
+signal new_combat_position(movement_vector: Vector2)
+signal damage(amount: int, target_positions)
 
 func _ready() -> void:
 	state_machine.init()
@@ -33,29 +35,45 @@ func _process(delta: float) -> void:
 		$".".set_modulate(Color(1, 1, 1, 1))
 		$".".modulate
 
-func _on_damage(damage_amount: int) -> void:
-	if !invulnerable:
+func _on_damage(damage_amount: int, target_positions) -> void:
+	print(target_positions)
+	print(combat_position)
+	if target_positions.has(combat_position):
 		damage_state = 6
 		current_health -= damage_amount
 		$".".set_modulate(Color(1, 0, 0, 1))
 		$".".modulate
 
-func attack(damage_quantity: int) -> void:
-	emit_signal("damage", damage_quantity)
-
+func attack(damage_quantity: int, target_positions) -> void:
+	emit_signal("damage", damage_quantity, target_positions)
 
 
 func movement(direction: String, movement_start: bool):
-	if movement_start:
-		emit_signal("start_movement", direction)
-	else:
-		match direction:
-			"BACKWARD": position.x -= 24
-			"FOREWARD": position.x += 24
-		emit_signal("finish_movement", direction)
+	match direction:
+		"BACKWARD":
+			if movement_start:
+				update_combat_position(Vector2(1, 0))
+				emit_signal("start_movement", direction)
+			else:
+				position.x -= 24
+				emit_signal("finish_movement", direction)
+		"FOREWARD":
+			if movement_start:
+				update_combat_position(Vector2(-1, 0))
+				emit_signal("start_movement", direction)
+			else:
+				position.x += 24
+				emit_signal("finish_movement", direction)
+
+func update_combat_position(movement_vector: Vector2):
+	combat_position += movement_vector
+	emit_signal("new_combat_position", movement_vector)
 
 func flip():
 	scale = scale * Vector2(-1, 1)
 
-func _on_corvant_damage(amount: int) -> void:
-	pass # Replace with function body.
+func jump():
+	update_combat_position(Vector2(0, 1))
+
+func land():
+	update_combat_position(Vector2(0, -1))
